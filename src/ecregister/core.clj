103977275731bc2745479d7d8850353a8a5d7703
Dmitry Suzdalev <dimsuz@gmail.com>
2013-12-15 15:39:55 +0400

    More correct looping and waiting for input

--- src/ecregister/core.clj
+++ src/ecregister/core.clj
@@ -2,10 +2,12 @@
   (:gen-class)
   (:use [seesaw.core])
   (:use [seesaw.mig])
+  (:use [seesaw.widgets.log-window])
+  (:require [clojure.string :refer [blank?]])
   (:require [clojure.core.async :refer [chan alts!! timeout thread]])
   )
-(require '[clojure.core.async :refer [chan alts!! timeout thread put!]]
- )
+; (require '[clojure.core.async :refer [chan alts!! alts! timeout thread put! go-loop]])
+; (require '[clojure.string :refer [blank?]])
 
 (defn build-avatars-tab []
   (let [bg-stamp-pos (button-group)
@@ -15,16 +17,33 @@
                       [(label "Положение штампа:")]
                       [(flow-panel :items [(radio :id :top :text "Сверху" :group bg-stamp-pos)
                                            (radio :id :bottom :text "Снизу" :group bg-stamp-pos)]) "wrap"]
-                      [(button :text "Проштамповать") ""]
-                      [(label :id :log :text "Some log") ""]])]
+                      [(button :text "Проштамповать") "wrap"]
+                      [(scrollable (log-window :id :log
+                                               :rows 8
+                                               :columns 80
+                                               ))
+                       "span"]])]
     (let [te-name (select form [:#username])
           lb-log (select form [:#log])
           c (chan)]
-      (listen te-name :document (fn [e]
-                                  (config! lb-log :text "starting wait...")
-                                  (thread (let [[v ch] (alts!! [c (timeout 1000)])]
-                                            (config! lb-log :text (str "wait ended. read " v))))
-                                  )))
+      (listen te-name
+              :document (fn [e]
+                          (put! c (text te-name)))
+              :focus-gained (fn [e]
+                             (go-loop []
+                                      (let [[v ch] (alts! [c (timeout 1000)])]
+                                        (if (not= v "stop")
+                                          (do
+                                            (if (not (blank? v))
+                                              (log lb-log (str "read " v ", waiting continues...\n"))
+                                              (log lb-log (str "timeout reached, getting avatar for " (text te-name) "\n")))
+                                            (recur))
+                                          (log lb-log "exiting loop.")))))
+              :focus-lost (fn [e]
+                            (log lb-log "sending stop\n")
+                            (put! c "stop")))
+
+      )
       form
       )
     )
